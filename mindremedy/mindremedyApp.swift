6bea830afaa532f27b14cffe2db5046a901100a0
Jonas Allen <jonas@dorja.com>
2026-09-14 15:50:23 +1000

    Initial Commit

---
+++ mindremedy/mindremedyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindremedyApp.swift
+//  mindremedy
+//
+//  Created by Jonas Allen on 14/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct mindremedyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
